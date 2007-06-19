d53d2cc5523e27032af54e2cc7be2fddbe93a44c
John Goerzen <jgoerzen@complete.org>
2007-06-19 06:39:01 +0000

    Address some of Mike's comments

--- examples/ch05/eqclasses.hs
+++ examples/ch05/eqclasses.hs
@@ -59,11 +59,21 @@ instance Show Color where
 {-- /snippet show --}
 {-- snippet read --}
 instance Read Color where
+    -- readsPrec is the main function for parsing input
     readsPrec _ value = 
+        -- We pass tryParse a list of pairs.  Each pair has a string
+        -- and the desired return value.  tryParse will try to match
+        -- the input to one of these strings.
         tryParse [("Red", Red), ("Green", Green), ("Blue", Blue)]
-        where tryParse [] = []
+        where tryParse [] = []    -- If there is nothing left to try, fail
               tryParse ((attempt, result):xs) =
+                      -- Compare the start of the string to be parsed to the
+                      -- text we are looking for.
                       if (take (length attempt) value) == attempt
+                         -- If we have a match, return the result and the
+                         -- remaining input
                          then [(result, drop (length attempt) value)]
+                         -- If we don't have a match, try the next pair
+                         -- in the list of attempts.
                          else tryParse xs
 {-- /snippet read --}
