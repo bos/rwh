7afabf126e33e6bec2f6c1e5fc9b1fd5f2a0d03c
John Goerzen <jgoerzen@complete.org>
2007-07-10 05:26:50 +0000

    Updates

---
+++ examples/ch06/actions.hs
@@ -0,0 +1,11 @@
+{-- snippet all --}
+-- ch06/actions2.hs
+
+numbers :: [Int]
+numbers = [1..10]
+
+main = do str2action "Start of the program"
+          printitall
+          str2action "Done!"
+{-- /snippet all --}
+
