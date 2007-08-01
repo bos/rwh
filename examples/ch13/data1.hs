99e2111b483aee01d0647f77ade25270d7a33c0d
John Goerzen <jgoerzen@complete.org>
2007-08-01 05:19:30 +0000

    Checkpointing

--- examples/ch13/data1.hs
+++ examples/ch13/data1.hs
@@ -1,3 +1,16 @@
+{-- snippet useless --}
+data Silly = Foo
+{-- /snippet useless --}
+
+{-- snippet silly2 --}
+data Silly2 = Foo2
+     deriving (Eq, Read, Show)
+
+toSilly2 :: a -> Silly2
+toSilly2 _ = Foo2
+{-- /snippet silly2 --}
+
+
 {-- snippet all --}
 data Color = Red | Blue | Green
 {-- /snippet all --}
