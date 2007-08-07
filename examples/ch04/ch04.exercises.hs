06a24bb6e1560119c5a3bd311959191ceb049406
Bryan O'Sullivan <bos@serpentine.com>
2007-08-07 05:57:32 +0000

    Explicit recursion, structural induction.

--- examples/ch04/ch04.exercises.hs
+++ examples/ch04/ch04.exercises.hs
@@ -1,3 +1,4 @@
+import Data.Char (ord)
 import Prelude hiding (concat, takeWhile)
 
 {-- snippet concat --}
@@ -19,3 +20,40 @@ groupBy f = foldr step []
     where step x [] = [[x]]
           step x ((y:ys):zs) | f x y     = (x:y:ys):zs
                              | otherwise = [x]:(y:ys):zs
+
+-- Idea courtesy of William Lee Irwin.
+{-- snippet asInt_fold --}
+asInt_fold :: String -> Int
+{-- /snippet asInt_fold --}
+asInt_fold ('-':xs) = negate (asInt' xs)
+asInt_fold xs = asInt_fold' xs
+
+asInt_fold' [] = error "empty string"
+asInt_fold' xs = foldr step 0 xs
+    where step c n
+              | c `elem` ['0'..'9'] = let n' = n * 10 + ord c - zero
+                                      in if n' < n
+                                         then error "numeric overflow"
+                                         else n'
+              | otherwise = error ("non-digit " ++ show c)
+          zero = ord '0'
+
+{-- snippet asInt_either --}
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+{-- /snippet asInt_either --}
+asInt_either ('-':xs) = case asInt_either' xs of
+                          Left err -> Left err
+                          Right val -> Right (negate val)
+asInt_either xs = asInt_either' xs
+
+asInt_either' [] = Left "empty string"
+asInt_either' xs = foldr step (Right 0) xs
+    where step c (Right n)
+              | c `elem` ['0'..'9'] = let n' = n * 10 + ord c - zero
+                                      in if n' < n
+                                         then Left "numeric overflow"
+                                         else Right n'
+              | otherwise = Left ("non-digit " ++ show c)
+          step _ err = err
+          zero = ord '0'
