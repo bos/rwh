cec7d243ca5d499d3e01a6e3e8419a5f61e6626e
Bryan O'Sullivan <bos@serpentine.com>
2007-08-24 06:47:11 +0000

    Missed some files.

---
+++ examples/ch04/Partial.hs
@@ -0,0 +1,14 @@
+import Data.List
+
+{-- snippet isInAny --}
+isInAny needle haystack = any inSequence haystack
+    where inSequence s = needle `isInfixOf` s
+{-- /snippet isInAny --}
+
+{-- snippet isInAny2 --}
+isInAny2 needle haystack = any (\s -> needle `isInfixOf` s) haystack
+{-- /snippet isInAny2 --}
+
+{-- snippet isInAny3 --}
+isInAny3 needle haystack = any (isInfixOf needle) haystack
+{-- /snippet isInAny3 --}
