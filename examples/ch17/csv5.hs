24e323b9f80c06035a1f0cddec9e85b98f6fa39d
John Goerzen <jgoerzen@complete.org>
2007-09-05 09:56:52 +0000

    Parsec work

--- examples/ch17/csv5.hs
+++ examples/ch17/csv5.hs
@@ -3,8 +3,13 @@ import Text.ParserCombinators.Parsec
 
 csvFile = endBy line eol
 line = sepBy cell (char ',')
-cell = many (noneOf ",\n")
-eol = char '\n'
+cell = many (noneOf ",\n\r")
+
+{-- snippet eol --}
+eol = 
+    do char '\n'
+       char '\r' <|> return '\n'
+{-- /snippet eol --}
 
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
--- examples/ch17/csv6.hs
+++ examples/ch17/csv6.hs
@@ -3,8 +3,14 @@ import Text.ParserCombinators.Parsec
 
 csvFile = endBy line eol
 line = sepBy cell (char ',')
-cell = many (noneOf ",\n")
-eol = char '\n'
+cell = many (noneOf ",\n\r")
+
+{-- snippet eol --}
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+{-- /snippet eol --}
 
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
