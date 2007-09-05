6315e00a6b5e1162922a7ae51aa845cfa4dab0c9
John Goerzen <jgoerzen@complete.org>
2007-09-05 10:48:24 +0000

    Build fixes

--- examples/ch17/csv6.hs
+++ examples/ch17/csv6.hs
@@ -1,15 +1,15 @@
+{-- snippet all --}
 import Text.ParserCombinators.Parsec
 
 csvFile = endBy line eol
 line = sepBy cell (char ',')
 cell = many (noneOf ",\n\r")
 
-{-- snippet eol --}
 eol =   try (string "\n\r")
     <|> try (string "\r\n")
     <|> string "\n"
     <|> string "\r"
-{-- /snippet eol --}
 
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
+{-- /snippet all --}
