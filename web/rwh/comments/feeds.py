64c4ae756b734058b07ca3a846102cac6d9a4f07
Bryan O'Sullivan <bos@serpentine.com>
2007-09-12 20:40:08 +0000

    Provide more metadata in a feed.

--- web/rwh/comments/feeds.py
+++ web/rwh/comments/feeds.py
@@ -4,8 +4,17 @@ from rwh.comments.models import Comment, Element
 
 class AllComments(Feed):
     feed_type = Atom1Feed
-    title = 'All recent comments for book.realworldhaskell.org'
-    link = '/book/'
+    title = 'Real World Haskell: recent comments'
+    subtitle = ('Recent comments on the text of &#8221;Real World '
+                'Haskell&#8220;, from our readers')
+    link = '/feeds/allcomments/'
+    author_name = 'Our readers'
 
     def items(self):
         return Comment.objects.order_by('-date')[:20]
+
+    def item_author_name(self, obj):
+        return obj.submitter_name
+
+    def item_pubdate(self, obj):
+        return obj.date
