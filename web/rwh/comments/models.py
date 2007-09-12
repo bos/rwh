42cf2351b1915b1d1ae499e888f14cadaa691dc4
Bryan O'Sullivan <bos@serpentine.com>
2007-09-12 05:52:01 +0000

    Store more information about elements, namely the section title. Add extra metadata to make the admin interface and code clearer.

--- web/rwh/comments/models.py
+++ web/rwh/comments/models.py
@@ -5,11 +5,14 @@ mutable = True
 
 class Element(models.Model):
     class Admin:
-        search_fields = ['id', 'chapter']
-        list_filter = ['chapter']
+        search_fields = ['id', 'section']
+        list_filter = ['section', 'title']
 
-    id = models.CharField(max_length=64, editable=False, primary_key=True)
-    chapter = models.CharField(max_length=64, editable=False, db_index=True)
+    id = models.CharField('ID attribute', max_length=64, editable=False,
+                          primary_key=True)
+    section = models.CharField('Section ID', max_length=64, editable=False,
+                               db_index=True)
+    title = models.CharField('Section title', max_length=256, editable=False)
 
     def __unicode__(self):
         return self.id
@@ -20,17 +23,25 @@ class Comment(models.Model):
         date_hierarchy = 'date'
         list_filter = ['date']
 
-    element = models.ForeignKey(Element)
-    comment = models.TextField(editable=mutable)
-    submitter_name = models.CharField(max_length=64)
-    submitter_url = models.URLField(blank=True, editable=mutable)
-    ip = models.IPAddressField(editable=mutable)
+    element = models.ForeignKey(Element,
+        help_text='ID of paragraph that was commented on')
+    comment = models.TextField(editable=mutable,
+        help_text='Text of submitted comment (do not modify)')
+    submitter_name = models.CharField('Submitter', max_length=64,
+        help_text='Self-reported name of submitter (may be bogus)')
+    submitter_url = models.URLField('URL', blank=True, editable=mutable,
+        help_text='Self-reported URL of submitter (may be empty or bogus)')
+    ip = models.IPAddressField('IP address', editable=mutable,
+        help_text='IP address from which comment was submitted')
     date = models.DateTimeField('date submitted', auto_now=True,
                                 auto_now_add=True)
-    reviewed = models.BooleanField()
+    reviewed = models.BooleanField(
+        help_text='Has this comment been reviewed by an author?')
+    hidden = models.BooleanField(default=False, db_index=True,
+        help_text='Has this comment been hidden from public display?')
 
     def __unicode__(self):
         return self.comment[:32]
 
     def get_absolute_url(self):
-        return '/html/%s.html#%s' % (self.element.chapter, self.element.id)
+        return '/html/%s.html#%s' % (self.element.section, self.element.id)
