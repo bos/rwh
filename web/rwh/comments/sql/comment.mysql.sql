0144cedd02fc6efa2fcc38e4343e20f9817f74d5
Bryan O'Sullivan <bos@serpentine.com>
2007-09-27 04:00:07 +0000

    Ensure that created tables use the right charset/collation.

---
+++ web/rwh/comments/sql/comment.mysql.sql
@@ -0,0 +1,2 @@
+alter table comments_comment convert to character set utf8 collate utf8_bin;
+alter table comments_comment default character set utf8 collate utf8_bin;
---
+++ web/rwh/comments/sql/element.mysql.sql
@@ -0,0 +1,2 @@
+alter table comments_element convert to character set utf8 collate utf8_bin;
+alter table comments_element default character set utf8 collate utf8_bin;
