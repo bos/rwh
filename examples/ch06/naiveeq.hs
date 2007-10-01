5d4dd3335cd565e4185dfecf4107b63306b39cd9
John Goerzen <jgoerzen@complete.org>
2007-10-01 06:31:40 +0000

    Some initial changes here

--- examples/ch06/naiveeq.hs
+++ examples/ch06/naiveeq.hs
@@ -1,26 +1,18 @@
 {-- snippet color --}
-data Color = Red | Green | Blue
+data Color = Red | Green
 
 colorEq :: Color -> Color -> Bool
 colorEq Red Red = True
-colorEq Red _ = False
 colorEq Green Green = True
-colorEq Green _ = False
-colorEq Blue Blue = True
-colorEq Blue _ = False
+colorEq _ _ = False
 {-- /snippet color --}
 
 {-- snippet string --}
 stringEq :: [Char] -> [Char] -> Bool
 -- Match if both are empty
 stringEq [] [] = True
--- Evaluate when we have only one character in both
-stringEq [x] [y] = x == y
 -- If both start with the same char, check the rest
-stringEq (x:xs) (y:ys) = 
-    if x == y
-        then stringEq xs ys
-        else False
+stringEq (x:xs) (y:ys) = x == y && stringEq xs ys
 -- Everything else doesn't match
 stringEq _ _ = False
 {-- /snippet string --}
