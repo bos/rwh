ba661b0cb41ad2e44c303d4398615226aac989d2
John Goerzen <jgoerzen@complete.org>
2007-10-02 09:08:31 +0000

    More ch06 updates

--- examples/ch06/read.hs
+++ examples/ch06/read.hs
@@ -1,7 +1,7 @@
 {-- snippet read --}
 main = do
-        putStrLn "Please enter an integer:"
+        putStrLn "Please enter a Double:"
         inpStr <- getLine
-        let inpInt = (read inpStr)::Integer
+        let inpInt = (read inpStr)::Double
         putStrLn ("Twice " ++ show inpInt ++ " is " ++ show (inpInt * 2))
 {-- /snippet read --}
