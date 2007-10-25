41c7126ab862fc1836c36d942accc4ef28dd5f46
John Goerzen <jgoerzen@complete.org>
2007-10-25 05:05:46 +0000

    Final tweaks

--- examples/ch20/test.hs
+++
@@ -1,8 +0,0 @@
-import RunProcess
-
-main = do
-    runIO $ ("/tmp/foo.sh", [""]) -|- grep "^s" -|- ("cat", ["-"])
-    --runIO $ (("/tmp/foo.sh", [""]) -|- ("grep", ["^s"])) -|- ("cat", ["-"])
-    --runIO $ ("/tmp/foo.sh", [""]) -|- ("grep", ["^s"]) -|- ("wc", ["-l"])
-    --runIO $ ("/tmp/foo.sh", [""]) -|- ("grep", ["^s"])
-    --runIO $ ("ls", ["/usr/bin"]) -|- ("grep", ["^s"])
