6ad5b2ed5dbd70366c9554e570286f87657f94cc
Bryan O'Sullivan <bos@serpentine.com>
2008-01-06 07:06:07 +0000

    Beginnings of state monad

---
+++ examples/ch14/State.hs
@@ -0,0 +1,22 @@
+{-- snippet State --}
+newtype State s a = State {
+      runState :: s -> (a, s)
+    }
+{-- /snippet State --}
+
+{-- snippet returnState --}
+returnState :: a -> State s a
+returnState a = State $ \s -> (a, s)
+{-- /snippet returnState --}
+
+{-- snippet bindState --}
+bindState :: State s a -> (a -> State s b) -> State s b
+bindState m k = State $ \s -> let (a, s') = runState m s
+                              in runState (k a) s'
+{-- /snippet bindState --}
+
+{-- snippet Monad --}
+instance Monad (State s) where
+    return a = returnState a
+    m >>= k = bindState m k
+{-- /snippet Monad --}
