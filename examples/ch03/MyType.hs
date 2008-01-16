e0a686adc15a0c6605021823e5b850c0f1c413c7
Bryan O'Sullivan <bos@serpentine.com>
2008-01-16 22:43:01 +0000

    More example file renaming

--- examples/ch03/MyType.hs
+++
@@ -1,8 +0,0 @@
-{-- snippet MyType --}
-data MyType = MyConstructor Int String
-            deriving (Show)
-{-- /snippet MyType --}
-
-{-- snippet myValue --}
-myValue = MyConstructor 31337 "Creating a value!"
-{-- /snippet myValue --}
---
+++ examples/ch04/BookSection.hs
@@ -0,0 +1,8 @@
+{-- snippet BookSection --}
+data BookSection = Section Int String
+            deriving (Show)
+{-- /snippet BookSection --}
+
+{-- snippet fp --}
+fp = Section 5 "Functional programming"
+{-- /snippet fp --}
