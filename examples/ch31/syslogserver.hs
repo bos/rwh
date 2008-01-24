fa6e6142050c23bc29b574c3b4a8b4a7588b84de
John Goerzen <jgoerzen@complete.org>
2008-01-24 08:04:41 +0000

    Added syslogserver

---
+++ examples/ch31/syslogserver.hs
@@ -0,0 +1,116 @@
+-- ch31/syslogclient.hs
+
+import Data.Bits
+import Network.Socket
+import Network.BSD
+import Data.List
+
+data SyslogHandle = 
+    SyslogHandle {slSocket :: Socket,
+                  slProgram :: String,
+                  slAddress :: SockAddr}
+
+openlog :: HostName             -- ^ Remote hostname, or localhost
+        -> String               -- ^ Port number or name; 514 is default
+        -> String               -- ^ Name to log under
+        -> IO SyslogHandle      -- ^ Handle to use for logging
+openlog hostname port progname =
+    do -- Look up the hostname and port.  Either raises an exception
+       -- or returns a nonempty list.  First element in that list
+       -- is supposed to be the best option.
+       addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+       let serveraddr = head addrinfos
+
+       -- Establish a socket for communication
+       sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+
+       -- Save off the socket, program name, and server address in a handle
+       return $ SyslogHandle sock progname (addrAddress serveraddr)
+
+syslog :: SyslogHandle -> Facility -> Priority -> String -> IO ()
+syslog syslogh fac pri msg =
+    sendstr sendmsg
+    where code = makeCode fac pri
+          sendmsg = "<" ++ show code ++ ">" ++ (slProgram syslogh) ++
+                    ": " ++ msg ++ "\0"
+
+          -- Send until everything is done
+          sendstr :: String -> IO ()
+          sendstr [] = return ()
+          sendstr omsg = do sent <- sendTo (slSocket syslogh) omsg
+                                    (slAddress syslogh)
+                            sendstr (genericDrop sent omsg)
+          
+closelog :: SyslogHandle -> IO ()
+closelog syslogh = sClose (slSocket syslogh)
+
+{- | Priorities define how important a log message is. -}
+
+data Priority = 
+            DEBUG                   -- ^ Debug messages
+          | INFO                    -- ^ Information
+          | NOTICE                  -- ^ Normal runtime conditions
+          | WARNING                 -- ^ General Warnings
+          | ERROR                   -- ^ General Errors
+          | CRITICAL                -- ^ Severe situations
+          | ALERT                   -- ^ Take immediate action
+          | EMERGENCY               -- ^ System is unusable
+                    deriving (Eq, Ord, Show, Read, Enum)
+
+{- | Facilities are used by the system to determine where messages
+are sent. -}
+
+data Facility = 
+              KERN                      -- ^ Kernel messages
+              | USER                    -- ^ General userland messages
+              | MAIL                    -- ^ E-Mail system
+              | DAEMON                  -- ^ Daemon (server process) messages
+              | AUTH                    -- ^ Authentication or security messages
+              | SYSLOG                  -- ^ Internal syslog messages
+              | LPR                     -- ^ Printer messages
+              | NEWS                    -- ^ Usenet news
+              | UUCP                    -- ^ UUCP messages
+              | CRON                    -- ^ Cron messages
+              | AUTHPRIV                -- ^ Private authentication messages
+              | FTP                     -- ^ FTP messages
+              | LOCAL0                  
+              | LOCAL1
+              | LOCAL2
+              | LOCAL3
+              | LOCAL4
+              | LOCAL5
+              | LOCAL6
+              | LOCAL7
+                deriving (Eq, Show, Read)
+
+{- | We can't use enum here because the numbering is discontiguous -}
+codeOfFac :: Facility -> Int
+codeOfFac f = case f of
+                       KERN -> 0
+                       USER -> 1
+                       MAIL -> 2
+                       DAEMON -> 3
+                       AUTH -> 4
+                       SYSLOG -> 5
+                       LPR -> 6
+                       NEWS -> 7
+                       UUCP -> 8
+                       CRON -> 9
+                       AUTHPRIV -> 10
+                       FTP -> 11
+                       LOCAL0 -> 16
+                       LOCAL1 -> 17
+                       LOCAL2 -> 18
+                       LOCAL3 -> 19
+                       LOCAL4 -> 20
+                       LOCAL5 -> 21
+                       LOCAL6 -> 22
+                       LOCAL7 -> 23
+
+{- | Convert a facility and a priority into a syslog code -}
+makeCode :: Facility -> Priority -> Int
+makeCode fac pri =
+    let faccode = codeOfFac fac
+        pricode = fromEnum pri 
+        in
+          (faccode `shiftL` 3) .|. pricode
