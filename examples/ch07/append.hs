cb660cb7203cf663de3b4107d379a1b205a29aed
Bryan O'Sullivan <bos@serpentine.com>
2008-01-29 23:43:16 +0000

    Rename examples: ch07 to ch08

--- examples/ch07/append.hs
+++
@@ -1,7 +0,0 @@
-{-# OPTIONS_GHC -fno-implicit-prelude #-}
-
-{-- snippet append --}
-(++)         :: [a] -> [a] -> [a]
-[]     ++ ys = ys
-(x:xs) ++ ys = x : (xs ++ ys)
-{-- /snippet append --}
--- examples/ch07/glob-to-regexp/GlobRegexTiny.hs
+++
@@ -1,8 +0,0 @@
-globToRegex :: String -> String
-
-globToRegex cs = '^' : globToRegex' cs
-
-globToRegex' :: String -> String
-
-globToRegex' "" = "$"
-globToRegex' ('*':cs) = ".*" ++ globToRegex' cs
