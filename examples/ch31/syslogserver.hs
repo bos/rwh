c4e949beb026718921a1c84bd662e6405529d1cd
John Goerzen <jgoerzen@complete.org>
2008-01-29 00:15:08 +0000

    Bug fixing in syslog client and server

--- examples/ch31/syslogserver.hs
+++ examples/ch31/syslogserver.hs
@@ -4,23 +4,26 @@ import Data.Bits
 import Network.Socket
 import Network.BSD
 import Data.List
-import SyslogTypes
+
+type HandlerFunc = SockAddr -> String -> IO ()
 
 serveLog :: String              -- ^ Port number or name; 514 is default
          -> HandlerFunc         -- ^ Function to handle incoming messages
          -> IO ()
-serveLog port handler =
+serveLog port handlerfunc =
     do -- Look up the port.  Either raises an exception or returns
        -- a nonempty list.  
-       addrinfos <- getAddrInfo (defaultHints {addrFlags = [AI_PASSIVE]})
+       addrinfos <- getAddrInfo 
+                    (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
                     Nothing (Just port)
        let serveraddr = head addrinfos
        sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
        bindSocket sock (addrAddress serveraddr)
-       listen sock 5
        procMessages sock
     where procMessages sock =
               do (msg, _, addr) <- recvFrom sock 1024
-                 putStrLn $ (show addr) ++ ": " ++ msg
+                 handlerfunc addr msg
                  procMessages sock
 
+plainHandler addr msg = 
+    putStrLn $ "From " ++ show addr ++ ": " ++ msg
