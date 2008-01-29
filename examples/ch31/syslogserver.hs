a913b06da028ffc1df133c4343ef387c591bc748
John Goerzen <jgoerzen@complete.org>
2008-01-29 00:18:12 +0000

    Commented syslog server

--- examples/ch31/syslogserver.hs
+++ examples/ch31/syslogserver.hs
@@ -17,13 +17,26 @@ serveLog port handlerfunc =
                     (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
                     Nothing (Just port)
        let serveraddr = head addrinfos
+
+       -- Create a socket
        sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+
+       -- Bind it to the address we're listening to
        bindSocket sock (addrAddress serveraddr)
+
+       -- Loop forever processing incoming data.  Ctrl-C to abort.
        procMessages sock
     where procMessages sock =
-              do (msg, _, addr) <- recvFrom sock 1024
+              do -- Receive one UDP packet, maximum length 1024 bytes,
+                 -- and save its content into msg and its source
+                 -- IP and port into addr
+                 (msg, _, addr) <- recvFrom sock 1024
+                 -- Handle it
                  handlerfunc addr msg
+                 -- And process more messages
                  procMessages sock
 
+-- A simple handler that prints incoming packets
+plainHAndler :: HandlerFunc
 plainHandler addr msg = 
     putStrLn $ "From " ++ show addr ++ ": " ++ msg
