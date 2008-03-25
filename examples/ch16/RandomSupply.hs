5136dd5859aae590ded8d08f469e66e54f42aed5
Bryan O'Sullivan <bos@serpentine.com>
2008-03-25 06:17:25 +0000

    A supply of random numbers

---
+++ examples/ch16/RandomSupply.hs
@@ -0,0 +1,10 @@
+{-- snippet randomsIO --}
+import Supply
+import System.Random hiding (next)
+
+randomsIO :: Random a => IO [a]
+randomsIO =
+    getStdRandom $ \g ->
+        let (a, b) = split g
+        in (randoms a, b)
+{-- /snippet randomsIO --}
