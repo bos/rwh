5c6bfa80ee67800f7626f4b014896eb1475692b6
John Goerzen <jgoerzen@complete.org>
2008-03-25 07:34:07 +0000

    Fixed up maps and starting on functions as data

--- examples/ch14/funcrecs.hs
+++ examples/ch14/funcrecs.hs
@@ -1,15 +1,23 @@
 {-- snippet all --}
+-- ch14/funcrecs.hs
+
+{- | Our usual CustomColor type to play with -}
 data CustomColor =
   CustomColor {red :: Int,
                green :: Int,
                blue :: Int}
   deriving (Eq, Show, Read)
 
+{- | A new type that stores a name and a function.
+
+The function takes an Int, applies some computation to it, and returns
+the Int along with a CustomColor -}
 data FuncRec =
     FuncRec {name :: String,
              colorCalc :: Int -> (CustomColor, Int)}
 
 plus5func color x = (color, x + 5)
+
 purple = CustomColor 255 0 255
 
 plus5 = FuncRec {name = "plus5", colorCalc = plus5func purple}
