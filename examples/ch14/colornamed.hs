e39ecc35b2789082004871db466ddf663962bf4b
John Goerzen <jgoerzen@complete.org>
2008-03-25 06:52:30 +0000

    Change second CustomColor to CustomColor2 per reviewer comment

--- examples/ch14/colornamed.hs
+++ examples/ch14/colornamed.hs
@@ -1,26 +1,26 @@
 {-- snippet custom --}
-data CustomColor = 
-  CustomColor {red :: Int,
-               green :: Int,
-               blue :: Int}
+data CustomColor2 = 
+  CustomColor2 {red :: Int,
+                green :: Int,
+                blue :: Int}
   deriving (Eq, Show, Read)
 {-- /snippet custom --}
 
 {-- snippet changered --}
-changeRed :: CustomColor -> Int -> CustomColor
+changeRed :: CustomColor2 -> Int -> CustomColor2
 changeRed cc newRed = cc {red = newRed}
 {-- /snippet changered --}
 
 {-- snippet extract --}
-color2string :: CustomColor -> String
+color2string :: CustomColor2 -> String
 color2string cc =
     "red: " ++ show (red cc) ++ ", green: " ++ show (green cc) 
     ++ ", blue: " ++ show (blue cc)
 {-- /snippet extract --}
 
 {-- snippet extract2 --}
-color2string2 :: CustomColor -> String
-color2string2 (CustomColor red green blue) =
+color2string2 :: CustomColor2 -> String
+color2string2 (CustomColor2 red green blue) =
     "red: " ++ show red ++ ", green: " ++ show green ++ ", blue: "
     ++ show blue
 {-- /snippet extract2 --}
