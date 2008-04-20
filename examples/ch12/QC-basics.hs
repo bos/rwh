4b909bde43bcf1ad638613bad527dc704a203f0f
Don Stewart <dons@galois.com>
2008-04-20 23:59:14 +0000

    basic quickcheck ghci example

--- examples/ch12/QC-basics.hs
+++ examples/ch12/QC-basics.hs
@@ -6,14 +6,29 @@ import Data.List
 -- Simple model testing
 
 {-- snippet mysort --}
+qsort :: (Ord a) => [a] -> [a]
 qsort []     = []
 qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
     where lhs = filter (< x) xs
           rhs = filter (>= x) xs
 {-- /snippet mysort --}
 
--- Relating functions to other functions
+{-- snippet basics --}
+prop_sort_idempotent xs = qsort (qsort xs) == qsort xs
+{-- /snippet basics --}
 
--- Simple function properties
+{-- snippet relatives --}
+prop_minimum xs         = not (null xs) ==> head (qsort xs) == minimum xs
+
+prop_maximum xs         = not (null xs) ==> last (qsort xs) == maximum xs
+
+prop_append xs ys       = not (null xs) ==>
+                          not (null ys) ==>
+                          head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
+{-- /snippet relatives --}
+
+{-- snippet model --}
+prop_sort_model xs      = sort xs == qsort xs
+{-- /snippet model --}
 
 -- Generating random data
