7d68b023a8bbd452de278c689f9bf1c2db8f0655
John Goerzen <jgoerzen@complete.org>
2008-04-22 05:33:41 +0000

    Checkpointing PodMain.hs

--- examples/ch24/PodMain.hs
+++ examples/ch24/PodMain.hs
@@ -10,8 +10,29 @@ import System.Environment
 import Database.HDBC
 import Network.Socket(withSocketsDo)
 
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Glade
+
+data GUI = GUI {
+      mainWin :: Window,
+      mwAddBt :: Button,
+      mwUpdateBt :: Button,
+      mwDownloadBt :: Button,
+      mwFetchBt :: Button,
+      mwExitBt :: Button,
+      statusWin :: Dialog,
+      swOKBt :: Button,
+      swCancelBt :: Button,
+      swLabel :: Label}
+
+-- import Paths_Pod(getDataFileName)
+
 main = withSocketsDo $ handleSqlError $
-    do args <- getArgs
+    do initGUI                  -- Initialize GTK engine
+       gui <- loadGlade
+       connectGui gui
+       
+       args <- getArgs
        dbh <- connect "pod.db"
        case args of
          ["add", url] -> add dbh url
@@ -21,6 +42,28 @@ main = withSocketsDo $ handleSqlError $
                          download dbh
          _ -> syntaxError
        disconnect dbh
+
+loadGlade =
+    do Just xml <- xmlNew "podresources.glade"
+
+       -- Load main window
+       mw <- xmlGetWidget xml castToWindow "mainWindow"
+       -- When the close button is clicked...
+                  
+       onDestroy mainWin exitApp
+       
+       -- Load main window buttons
+
+       [mwAdd, mwUpdate, mwDownload, mwFetch, mwExit, swOK, swCancel] <-
+           mapM (xmlGetWidget xml castToButton)
+           ["addButton", "updateButton", "downloadButton",
+            "fetchButton", "exitButton", "okButton", "cancelButton"]
+       
+       sw <- xmlGetWidget xml castToDialog "statusDialog"
+       swl <- xmlGetWidget xml castToLabel "statusLabel"
+
+       return $ GUI mw mwAdd mwUpdate mwDownload mwFetch mwExit
+              sw swOK swCancel swl
 
 add dbh url = 
     do addPodcast dbh pc
