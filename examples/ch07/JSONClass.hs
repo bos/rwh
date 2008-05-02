48c5f04b47cdec328f8afba176da7edc9db2d3b9
Bryan O'Sullivan <bos@serpentine.com>
2008-05-02 03:38:11 +0000

    Get code compiling.

--- examples/ch07/JSONClass.hs
+++ examples/ch07/JSONClass.hs
@@ -4,55 +4,57 @@ module JSONClass
     (
       JSON(..)
     , JValue(..)
-    , JArray
-    , JObject
-    , fromJArray
+    , JAry
+    , JObj
+    , fromJAry
     , jarray
-    , fromJObject
+    , fromJObj
     , jobject
     ) where
 
 import Control.Arrow (second)
 import Control.Monad (liftM)
 
-newtype JArray a = JAry {
-      fromJArray :: [a]
+newtype JAry a = JAry {
+      fromJAry :: [a]
     } deriving (Eq, Ord, Show)
 
-jarray :: JSON a => [a] -> JArray a
+jarray :: JSON a => [a] -> JAry a
 jarray = JAry
 
-newtype JObject a = JObj {
-      fromJObject :: [(String, a)]
+newtype JObj a = JObj {
+      fromJObj :: [(String, a)]
     } deriving (Eq, Ord, Show)
 
-jobject :: JSON a => [(String, a)] -> JObject a
+jobject :: JSON a => [(String, a)] -> JObj a
 jobject = JObj
 
 data JValue = JString String
             | JNumber !Rational
-            | JObject (JObject JValue)
-            | JArray (JArray JValue)
+            | JObject (JObj JValue)
+            | JArray (JAry JValue)
             | JBool !Bool
             | JNull
               deriving (Eq, Ord, Show)
 
+type JSONError = String
+
 class JSON a where
     toJValue :: a -> JValue
-    fromJValue :: Monad m => JValue -> m a
+    fromJValue :: JValue -> Either JSONError a
 
 instance JSON JValue where
     toJValue = id
-    fromJValue = return
+    fromJValue = Right
 
-rationalToJValue :: (Monad m) => (Rational -> b) -> JValue -> m b
-rationalToJValue f (JNumber v) = return . f $ v
-rationalToJValue _ _ = fail "not a JSON number"
+rationalToJValue :: (Rational -> a) -> JValue -> Either JSONError a
+rationalToJValue f (JNumber v) = Right (f v)
+rationalToJValue _ _ = Left "not a JSON number"
 
 instance JSON String where
     toJValue = JString
-    fromJValue (JString s) = return s
-    fromJValue _ = fail "not a JSON string"
+    fromJValue (JString s) = Right s
+    fromJValue _ = Left "not a JSON string"
 
 instance JSON Int where
     toJValue = JNumber . toRational
@@ -70,23 +72,35 @@ instance JSON Rational where
     toJValue = JNumber
     fromJValue = rationalToJValue id
 
-instance (JSON a) => JSON (JArray a) where
-    toJValue = JArray . jarray . map toJValue . fromJArray
-    fromJValue (JArray a) = jarray `liftM` mapM fromJValue (fromJArray a)
-    fromJValue _ = fail "not a JSON array"
+mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
+mapEithers f (x:xs) = case mapEithers f xs of
+                        Left err -> Left err
+                        Right ys -> case f x of
+                                      Left err -> Left err
+                                      Right y -> Right (y:ys)
+mapEithers _ _ = Right []
 
-instance (JSON a) => JSON (JObject a) where
-    toJValue = JObject . jobject . map (second toJValue) . fromJObject
-    fromJValue (JObject o) = jobject `liftM` mapM unwrap (fromJObject o)
-        where unwrap (k, v) = fromJValue v >>= return . (,) k
-    fromJValue _ = fail "not a JSON object"
+instance (JSON a) => JSON (JAry a) where
+    toJValue = JArray . jarray . map toJValue . fromJAry
+    fromJValue (JArray a) = whenRight jarray (mapEithers fromJValue (fromJAry a))
+    fromJValue _ = Left "not a JSON array"
+
+instance (JSON a) => JSON (JObj a) where
+    toJValue = JObject . jobject . map (second toJValue) . fromJObj
+    fromJValue (JObject o) = whenRight jobject (mapEithers unwrap (fromJObj o))
+        where unwrap (k, v) = whenRight ((,) k) (fromJValue v)
+    fromJValue _ = Left "not a JSON object"
 
 instance JSON Bool where
     toJValue = JBool
-    fromJValue (JBool b) = return b
-    fromJValue _ = fail "not a JSON boolean"
+    fromJValue (JBool b) = Right b
+    fromJValue _ = Left "not a JSON boolean"
+
+whenRight :: (b -> c) -> Either a b -> Either a c
+whenRight _ (Left err) = Left err
+whenRight f (Right a) = Right (f a)
 
 instance (JSON a) => JSON (Maybe a) where
     toJValue = maybe JNull toJValue
-    fromJValue JNull = return Nothing
-    fromJValue v = Just `liftM` fromJValue v
+    fromJValue JNull = Right Nothing
+    fromJValue v = whenRight Just (fromJValue v)
