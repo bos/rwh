c7b200781aaad650e80d5163bfe74f7f1b98c197
Don Stewart <dons@galois.com>
2008-05-12 07:20:48 +0000

    Tweaks

--- examples/ch12/Arbitrary.hs
+++ examples/ch12/Arbitrary.hs
@@ -3,11 +3,19 @@ module Arbitrary where
 {-- snippet Class --}
 class Arbitrary a where
   arbitrary   :: Gen a
-  coarbitrary :: a -> Gen b -> Gen b
 {-- snippet /Class --}
+
+--  coarbitrary :: a -> Gen b -> Gen b
+
+{-- snippet IntroductionForms --}
+  elements :: [a] -> Gen a
+  choose   :: Random a => (a, a) -> Gen a
+  oneof    :: [Gen a] -> Gen a
+{-- snippet /IntroductionForms --}
 
 {-- snippet Instance --}
 instance Arbitrary Bool where
   arbitrary     = elements [True, False]
-  coarbitrary b = if b then variant 0 else variant 1
 {-- snippet /Instance --}
+
+--  coarbitrary b = if b then variant 0 else variant 1
