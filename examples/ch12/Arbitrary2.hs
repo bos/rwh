3b6561d7b607de7450e9e72b6a08be9d7b50028b
Don Stewart <dons@galois.com>
2008-05-12 07:22:38 +0000

    Typo

--- examples/ch12/Arbitrary2.hs
+++ examples/ch12/Arbitrary2.hs
@@ -5,5 +5,5 @@ instance Arbitrary Bool where
   arbitrary     = do
       n <- choose (0, 1::Int)
       return $ if n == 0 then False else True
-{-- snippet /Instance2 --}
+{-- /snippet Instance2 --}
 
