18cff40c17f0af26dadbe0fca2726860856c2951
Don Stewart <dons@galois.com>
2008-05-12 05:21:31 +0000

    More QuickCheck examples

--- examples/ch12/Prettify.hs
+++ examples/ch12/Prettify.hs
@@ -118,24 +118,45 @@ compact x = transform [x]
 {-- /snippet compact --}
 
 pretty :: Int -> Doc -> String
+{-- /snippet pretty.type --}
+
+{-- snippet pretty --}
 pretty width x = best 0 [x]
     where best col (d:ds) =
               case d of
                 Empty        -> best col ds
                 Char c       -> c :  best (col + 1) ds
                 Text s       -> s ++ best (col + length s) ds
-                Line         -> '\n' : best col ds
-          --    Line         -> '\n' : best 0 ds
+                Line         -> '\n' : best 0 ds
                 a `Concat` b -> best col (a:b:ds)
                 a `Union` b  -> nicest col (best col (a:ds))
                                            (best col (b:ds))
           best _ _ = ""
+{-- /snippet pretty --}
 
---        nicest col a b | (width - least) `fits` a = a
-          nicest col a b | min width col `fits` a = a
-                         | otherwise              = b
-          w `fits` x | w < 0 = False
-          w `fits` ""        = True
-          w `fits` ('\n':_)  = True
-          w `fits` (c:cs)    = (w - 1) `fits` cs
+{-- snippet nicest --}
+          nicest col a b | (width - least) `fits` a = a
+                         | otherwise                = b
+                         where least = min width col
+{-- /snippet nicest --}
 
+{-- snippet fits --}
+fits :: Int -> String -> Bool
+w `fits` _ | w < 0 = False
+w `fits` ""        = True
+w `fits` ('\n':_)  = True
+w `fits` (c:cs)    = (w - 1) `fits` cs
+{-- /snippet fits --}
+
+{-- snippet nest --}
+nest :: Int -> Doc -> Doc
+{-- /snippet nest --}
+nest = undefined
+
+{-- snippet fill --}
+fill :: Int -> Doc -> Doc
+{-- /snippet fill --}
+fill = undefined
+
+--instance Show Doc where
+--    show doc = pretty 80 doc
