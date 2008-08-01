316bb8299efa0979a57a33e483d9ab5cff17fed1
Don Stewart <dons@galois.com>
2008-08-01 23:56:24 +0000

    Address online comments about ch12

--- examples/ch12/minimum.hs
+++ examples/ch12/minimum.hs
@@ -1,4 +1,8 @@
 {-- snippet minimum --}
+head       :: [a] -> a
+head (x:_) = x
+head []    = error "Prelude.head: empty list"
+
 minimum    :: (Ord a) => [a] -> a
 minimum [] =  error "Prelude.minimum: empty list"
 minimum xs =  foldl1 min xs
